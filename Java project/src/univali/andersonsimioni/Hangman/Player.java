62ef30b1192517da0d8663ff71121ce66dbeb5a7
Bit <willian.souza.ti@gmail.com>
2020-10-18 16:10:43 -0300

    add skeleton for player

--- Java project/src/univali/andersonsimioni/Hangman/Player.java
+++ Java project/src/univali/andersonsimioni/Hangman/Player.java
@@ -1,4 +1,22 @@
 package univali.andersonsimioni.Hangman;
 
 public class Player {
+    private final String name;
+    private Integer score;
+
+    public Player(String name, Integer score) {
+        this.name = name;
+    }
+
+    public void addScore(Integer score){
+        this.score += score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
 }
---
+++ Java project/src/univali/andersonsimioni/UnitTest/Player.java
@@ -0,0 +1,6 @@
+package univali.andersonsimioni.UnitTest;
+
+public class Player {
+    public static void testPayer() {
+    }
+}
